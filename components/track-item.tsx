6dec60e950b09d7f4fdd2add15162af6b26f3159
NTKH <hn26677@gmail.com>
2025-04-16 00:00:43 +0700

    🚧:on progress

--- components/track-item.tsx
+++ components/track-item.tsx
@@ -9,12 +9,24 @@ function TrackItem({
 }: React.HTMLAttributes<HTMLDivElement>) {
     return (
         <div
-            className={cn('flex justify-between hover:bg-white/10', className)}
+            className={cn(
+                'flex h-[56px] items-center justify-between rounded-sm pr-4 pl-2 hover:bg-white/10',
+                className
+            )}
             {...props}
         >
-            <div className="flex flex-col gap-1">
-                <p className="text-white">Như Cách Anh Đã Từng Thôi</p>
-                <span className="text-neutral-400">HURRYKNG</span>
+            <div className="flex items-center gap-1">
+                <div className="">
+                    <img
+                        className="size-10 rounded-sm"
+                        src="/nadtt.jpg"
+                        alt=""
+                    />
+                </div>
+                <div className="flex flex-col">
+                    <p className="text-white">Như Cách Anh Đã Từng Thôi</p>
+                    <span className="text-neutral-400">HURRYKNG</span>
+                </div>
             </div>
 
             <div className=""></div>
